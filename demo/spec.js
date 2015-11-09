454d712f911e49cda80f544e966c8b6030ae8d4f
valeriy.tereshenko <tereshenko.valeriy.v@gmail.com>
2015-11-09 23:18:52 +0200

    проба нотации

--- demo/spec.js
+++ demo/spec.js
@@ -3,46 +3,69 @@
  */
 
 feature('add', 'Calculator')
-    .add(scenario('should be able to add 2 numbers together')
+    .add(steps('should be able to add 2 numbers together')
         .when('I enter "1"')
         .and('I add "2"')
         .then('I should get "3"'))
 
-    .add(scenario('should be able to add to a result of a previous addition')
+    .add(steps('should be able to add to a result of a previous addition')
         .given('I added "1" and "2"')
         .when('I add "3"')
         .then('I should get "6"'))
 
-    .add(scenario('should be able to add asynchronously')
+    .add(steps('should be able to add asynchronously')
         .when('I eventually add "1" and "2"')
         .then('I should get "3"'));
 
 
 /*
-feature('Contexts', 'bjc')
-    .add(scenario('Use steps from feature')
-        .given('Array', [1, 2, 3])
-        .when('Pop item')
-        .then('Has array', [1, 2]))
+ feature('Contexts', 'bjc')
+ .add(steps('Use steps from feature')
+ .given('Array', [1, 2, 3])
+ .when('Pop item')
+ .then('Has array', [1, 2]))
 
-    .add(scenario('Use steps from background')
-        .given('Object', {x:1, y:2})
-        .when('Remove item "x"')
-        .then('Object.x', undefined))
+ .add(steps('Use steps from background')
+ .given('Object', {x:1, y:2})
+ .when('Remove item "x"')
+ .then('Object.x', undefined))
 
-    .add(scenario('Use steps from group'))
+ .add(steps('Use steps from group'))
 
-    .add(scenario('Use steps priority'));
-*/
+ .add(steps('Use steps priority'));
+ */
 
 
 feature('Examples', 'bjc')
-    .add(scenario('Repeat for each example'))
+    .add(steps('Repeat for each example'))
 
-    .add(scenario('Example table with object'));
+    .add(steps('Example table with object'));
 
 
 feature('Grouping', 'bjc')
-    .add(scenario('Same feature name in group and without it'))
+    .add(steps('Same feature name in group and without it'))
 
-    .add(scenario('Same feature name in different groups'));
+    .add(steps('Same feature name in different groups'));
+
+
+scenario('отдельно стоящий сценарий')
+    .add(steps('name_1')
+        .given('что-то')
+        .when('что-то')
+        .then('что-то'))
+    .add(steps('name_2')
+        .when()
+        .then())
+    .add(steps('name_3')
+        .when()
+        .then())
+    .addGraph([
+        ['init',
+            ['p1', 'name_1'],
+            ['p1', 'name_2']],
+        ['p1',
+            ['p2', 'name_1'],
+            ['end', 'name_3']],
+        ['p2',
+            ['end', 'name_2']]
+    ]);
